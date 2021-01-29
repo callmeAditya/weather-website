857e1a0c54f61b3ce3093a8202eeb33fdd1ba7c1
callmeAditya <rana073aditya@gmail.com>
2021-01-29 11:37:16 +0530

    init commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port= process.env.PORT || 3000
+const port= process.env.PORT || 3100
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -97,5 +97,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port 3000.')
+    console.log('Server is up on port 3100.')
 })
